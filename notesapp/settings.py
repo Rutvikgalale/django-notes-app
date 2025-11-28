19734015a649a3f6b4e20feaf1b4ff9fa6b691a3
rutvik <rutvik@localhost.localdomain>
2025-11-28 13:53:48 +0530

    edited settings.py file
    Signed-off-by: rutvik <rutvik@localhost.localdomain>

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
   #      'ENGINE': 'django.db.backends.mysql',
    #     'NAME': os.getenv("DB_NAME"),
     #    'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
+#        'PASSWORD': os.getenv("DB_PASSWORD"),
   #      'HOST': os.getenv("DB_HOST"),
   #      'PORT': os.getenv("DB_PORT"),
   #  }
